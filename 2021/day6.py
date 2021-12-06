95ee552fdfe70db9ae8dc0c08cd0423a2f556a9d
Nabih <estefan.nabih@gmail.com>
2021-12-06 01:06:32 -0500

    cleaning and shortening day6

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,34 +1,14 @@
-## Parsing
+today = [0,0,0,0,0,0,0,0,0]
+
 with open('day6.txt') as f:
-    lines = f.readlines()
+    for i in f.readlines()[0]:
+        if i != ',' and i !='\n':
+            today[int(i)] += 1
 
-fish = []
-for i in lines[0]:
-    if i != ',' and i !='\n':
-        fish.append(int(i))
-
-days = 256 #80
-
-today = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
-tomorrow = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
-for i in fish:
-    today[i] += 1
-
-for i in range(0,days):
-    tomorrow[8] = today[0]
-    tomorrow[0] = today[1]
-    tomorrow[1] = today[2]
-    tomorrow[2] = today[3]
-    tomorrow[3] = today[4]
-    tomorrow[4] = today[5]
-    tomorrow[5] = today[6]
-    tomorrow[6] = today[7] + today[0]
-    tomorrow[7] = today[8]
-
-    today = tomorrow
-    tomorrow = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
-
-total = 0
-for i in range(0,9):
-    total += today[i]
-print(total)
+for i in range(0,256):
+    new_fish = today[0]
+    today[0:8] = today[1:]
+    today[8] = new_fish
+    today[6] += new_fish
+    
+print(sum(today))
