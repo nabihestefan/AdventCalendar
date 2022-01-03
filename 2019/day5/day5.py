112b533efc23d18fb496c1a6cffb78b95375d350
Nabih <estefan.nabih@gmail.com>
2022-01-03 15:03:26 -0500

    days 1-5 + intcode computer solutions

---
+++ 2019/day5/day5.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('../')
+from intCode import *
+files = ['input.txt', 'inputTest.txt']
+## Parsing
+with open(files[0], 'r') as f:
+    ints =  [int(a) for a in f.readlines()[0].split(",")]
+
+print("Part 1: ", IntCode(ints[:], [1]).execute())
+print("Part 1: ", IntCode(ints[:], [5]).execute())
