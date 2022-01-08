5328c1915cd6141afee6333971fa7db173d908a0
Nabih <estefan.nabih@gmail.com>
2022-01-08 13:38:57 -0500

    days 1-5 solutions

--- 2018/day3/day3.py
+++ 2018/day3/day3.py
@@ -1,3 +1,30 @@
 files = ['input.txt', 'inputTest.txt']
 ## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open(files[0], 'r') as f:
+    lines =  [a.strip() for a in f.readlines()]
+
+squares = []
+for line in lines:
+    left = int(line[line.index("@")+1:line.index(",")])
+    top = int(line[line.index(",")+1:line.index(":")])
+    width = int(line[line.index(":")+1:line.index("x")])
+    height = int(line[line.index("x")+1:])
+    square = set()
+    for x in range(left,left+width):
+        for y in range(top, top+height):
+            square.add((x,y))
+    squares.append(square)
+
+overlapped = set()
+for ind, s1 in enumerate(squares):
+    singleO = 0
+    for s2 in squares:
+        if s1==s2: continue
+        for point in s1&s2:
+            overlapped.add(point)
+            singleO += 1
+    if singleO == 0:
+        id = ind+1
+
+print("Part 1: ", len(overlapped))
+print("Part 2: ", id)
--- 2018/day5/day5.py
+++ 2018/day5/day5.py
@@ -1,3 +1,28 @@
 files = ['input.txt', 'inputTest.txt']
 ## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open(files[0], 'r') as f:
+    polymer = f.readlines()[0].strip()
+
+units = {}
+for i in polymer:
+    if i.lower() not in units.keys(): units[i.lower()] = -1
+
+def calcLen(polymer):
+    while True:
+        for i in range(len(polymer)-1):
+            if ord(polymer[i]) - 32 == ord(polymer[i+1]) or ord(polymer[i]) + 32 == ord(polymer[i+1]):
+                polymer = polymer[:i] + polymer[i+2:]
+                break
+        if i == len(polymer)-2: break
+    return len(polymer)
+
+def clean(polymer, i):
+    polymer = polymer.replace(i ,"")
+    polymer = polymer.replace(i.upper() ,"")
+    return polymer
+
+
+for i in units: units[i] = calcLen(clean(polymer[:], i))
+
+print("Part 1: ", calcLen(polymer))
+print("Part 2: ", min(units.values()))
