4bea78fee3546641de5f3f8a9be43eb1eb9458ee
Nabih <estefan.nabih@gmail.com>
2022-01-08 16:49:24 -0500

    day6-10 solutions

--- 2018/day7/day7.py
+++ 2018/day7/day7.py
@@ -1,3 +1,30 @@
+import networkx as nx
 files = ['input.txt', 'inputTest.txt']
 ## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open(files[0], 'r') as f:
+    lines =  [a.split() for a in f.readlines()]
+
+g = nx.DiGraph([(line[1], line[7]) for line in lines])
+print("Part 1: ", "".join(nx.lexicographical_topological_sort(g)))
+
+workers = 5
+def timeForTask(c):
+    return ord(c)-4
+
+t = 0
+taskTimes = []
+tasks = []
+while tasks or g:
+    openTasks = [a for a in g if a not in tasks and g.in_degree(a) == 0]
+    if openTasks and len(tasks) < workers:
+        task = min(openTasks)
+        taskTimes.append(timeForTask(task))
+        tasks.append(task)
+    else:
+        minTime = min(taskTimes)
+        finished = [tasks[i] for i,v in enumerate(taskTimes) if v == minTime]
+        taskTimes = [v-minTime for v in taskTimes if v > minTime]
+        tasks = [t for t in tasks if t not in finished]
+        t += minTime
+        g.remove_nodes_from(finished)
+print("Part 2: ", t)
--- 2018/day9/day9.py
+++ 2018/day9/day9.py
@@ -1,3 +1,40 @@
+#Only importing cause running without is too long
+from collections import deque, defaultdict
 files = ['input.txt', 'inputTest.txt']
 ## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open(files[0], 'r') as f:
+    words = f.readlines()[0].strip().split()
+    players = int(words[0])
+    marbles = int(words[6])
+
+# No libraries, take 5+ minutes to run
+def OGrun(marbles):
+    scores = {}
+    circle = [0]
+    for marble in range(1, marbles+1):
+        print(marble, marbles)
+        if marble%23 == 0:
+            circle = circle[-7:] + circle[:-7]
+            scores[marble%players] = scores.get(marble%players, 0) + marble + circle.pop()
+            circle = circle[1:] + circle[:1]
+        else:
+            circle = circle[1:] + circle[:1]
+            circle.append(marble)
+    return max(scores.values())
+
+#libraries, takes ~2 secs to run
+def run(marbles):
+    scores = defaultdict(int)
+    circle = deque([0])
+    for marble in range(1, marbles+1):
+        if marble%23 == 0:
+            circle.rotate(7)
+            scores[marble%players] += marble + circle.pop()
+            circle.rotate(-1)
+        else:
+            circle.rotate(-1)
+            circle.append(marble)
+    return max(scores.values())
+
+print("Part 1: ", run(marbles))
+print("Part 2: ", run(marbles*100))
