61b875397a16d65e9e59df6ad71ce5a65d42a62f
Nabih <estefan.nabih@gmail.com>
2022-02-01 23:20:08 -0500

    solution day 2

--- 2017/day02/day2.py
+++ 2017/day02/day2.py
@@ -1,3 +1,22 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f:
+    nums =  [sorted(map(int, a.strip().split("\t"))) for a in f.readlines()]
+
+
+
+def run(nums, partTwo):
+    checksum = 0
+    for row in nums:
+        if not partTwo:
+            checksum += row[-1] - row[0]
+        else:
+            found = False
+            while not found:
+                div = row.pop(0)
+                for i in row:
+                    if i%div==0:
+                        checksum += i//div
+                        found = True
+    return checksum
+
+print("Part 1: ", run(nums, False))
+print("Part 2: ", run(nums, True))
