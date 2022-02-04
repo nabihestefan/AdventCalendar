a6239fb9080293a109ca0c4c20356d20de995e2c
Nabih <estefan.nabih@gmail.com>
2022-02-03 23:11:34 -0500

    day 04 solution

--- 2017/day04/day4.py
+++ 2017/day04/day4.py
@@ -1,3 +1,10 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f:
+    lines =  [a.strip().split(" ") for a in f.readlines()]
+
+def run(partTwo):
+    for line in lines:
+        if partTwo: line = ["".join(sorted(a)) for a in line]
+        if len(line) == len(set(line)): yield 1
+
+print("Part 1: ", sum(run(False)))
+print("Part 2: ", sum(run(True)))
