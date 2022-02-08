715a372be7996c7a6833a8a2fefeeea97aa74873
Nabih <estefan.nabih@gmail.com>
2022-02-07 23:17:16 -0500

    day 5 solution

--- 2017/day05/day5.py
+++ 2017/day05/day5.py
@@ -1,3 +1,14 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f: nums = [int(a) for a in f.readlines()]
+
+def run(nums, part2):
+    steps, i = 0,0
+    while i < len(nums):
+        steps += 1
+        inst = nums[i]
+        nums[i] = inst + 1 if inst < 3 or not part2 else inst -1
+        i += inst
+    return steps
+
+
+print("Part 1: ", run(nums[:], False))
+print("Part 2: ", run(nums[:], True))
