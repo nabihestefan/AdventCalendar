09c262f6405fa66c062a2394af55f3d9e52fc298
Nabih <estefan.nabih@gmail.com>
2022-02-13 23:55:22 -0500

    day 8 solution

--- 2017/day08/day8.py
+++ 2017/day08/day8.py
@@ -1,3 +1,15 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f: lines =  [a.strip() for a in f.readlines()]
+
+def run():
+    regs, maxTotal = dict(), 0
+    for i in lines:
+        words = i.split()
+        string = str(regs.get(words[4], 0)) + words[5] + words[6]
+        if eval(string):
+            val = int(words[2]) * -1 if words[1] == "dec" else int(words[2])
+            regs[words[0]] = regs.get(words[0], 0) + val
+            maxTotal = max(maxTotal, regs[words[0]])
+
+    return max(regs.values()), maxTotal
+
+print("Part 1 & 2: ", run())
