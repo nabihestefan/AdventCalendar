b54cacbe6bf340aa19bd0e2fe5d6136e524f3a0e
Nabih <estefan.nabih@gmail.com>
2022-02-14 23:23:45 -0500

    day 9 solutions

--- 2017/day09/day9.py
+++ 2017/day09/day9.py
@@ -1,3 +1,22 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f: lines =  f.readlines()[0].strip()
+
+def run():
+    depth = garbage = total = i = 0
+    valid = True
+    while i < len(lines):
+        if lines[i] == "!":
+            i += 2
+            continue
+        if valid:
+            if lines[i] == "<": valid = False
+            if lines[i] == "{": depth += 1
+            if lines[i] == "}":
+                total += depth
+                depth -= 1
+        else:
+            if lines[i] == ">": valid = True
+            else: garbage += 1
+        i += 1
+    return total, garbage
+
+print("Part 1 & 2: ", run())
