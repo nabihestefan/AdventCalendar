3add8d7ce87540589a28b29aa3c996d4355da9fa
Nabih <estefan.nabih@gmail.com>
2022-02-16 00:27:43 -0500

    day 10 solution

--- 2017/day10/day10.py
+++ 2017/day10/day10.py
@@ -5,7 +5,6 @@ def reverse(values, pointer, length):
     shifted = shifted[:length][::-1] + shifted[length:]
     return shifted[-pointer:] + shifted[:-pointer]
 
-
 def part1():
     nums =  map(int,lines.split(","))
     values = list(range(256))
@@ -18,12 +17,12 @@ def part1():
     return values[0] * values[1]
 
 def part2():
-    ascii = map(ord, lines) + [17, 31, 73, 47, 23]
+    nums = map(ord, lines) + [17, 31, 73, 47, 23]
     skip = pos = 0
     values = list(range(256))
 
     for _ in range(64):
-        for length in ascii:
+        for length in nums:
             values = reverse(values, pos, length)
             pos = (pos + length + skip) % len(values)
             skip += 1
@@ -31,7 +30,7 @@ def part2():
     dense = ""
     for i in range(16):
         num = 0
-        for j in range(16): num = num ^ values[i*16 + j]
+        for j in range(16): num ^= values[i*16 + j]
         dense += hex(num)[2:] if len(hex(num)) == 4 else "0" + hex(num)[2:]
 
     return dense
