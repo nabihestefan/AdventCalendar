d146220389de5cd52f8d20aac2bd35f24c7404b5
Nabih <estefan.nabih@gmail.com>
2022-02-16 23:26:16 -0500

    day 11 solution

--- 2017/day11/day11.py
+++ 2017/day11/day11.py
@@ -1,3 +1,11 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f: lines = f.readlines()[0].strip().split(",")
+
+def run():
+    maxVal = x = y = 0
+    dirs = {"n":(0,-1), "ne":(0.5,-0.5), "nw":(-0.5,-0.5), "s":(0,1), "se":(0.5, 0.5), "sw":(-0.5,0.5)}
+    for i in lines:
+        x,y = x + dirs[i][0], y + dirs[i][1]
+        maxVal = max(maxVal , abs(x) + abs(y))
+    return int(abs(x)+abs(y)), int(maxVal)
+
+print("Part 1 & 2: ", run())
