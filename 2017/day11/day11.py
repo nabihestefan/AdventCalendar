717dee60b50383eaea1b5c43796a4c9ee7907188
Nabih <estefan.nabih@gmail.com>
2022-02-16 23:27:52 -0500

    day 11 solution

--- 2017/day11/day11.py
+++ 2017/day11/day11.py
@@ -2,7 +2,8 @@ with open('input.txt', 'r') as f: lines = f.readlines()[0].strip().split(",")
 
 def run():
     maxVal = x = y = 0
-    dirs = {"n":(0,-1), "ne":(0.5,-0.5), "nw":(-0.5,-0.5), "s":(0,1), "se":(0.5, 0.5), "sw":(-0.5,0.5)}
+    dirs = {"n":(0,-1), "ne":(0.5,-0.5), "nw":(-0.5,-0.5),
+            "s":(0,1), "se":(0.5, 0.5), "sw":(-0.5,0.5)}
     for i in lines:
         x,y = x + dirs[i][0], y + dirs[i][1]
         maxVal = max(maxVal , abs(x) + abs(y))
