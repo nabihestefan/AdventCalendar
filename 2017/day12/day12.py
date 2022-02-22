375e2a9ca27c38e9334ecfb6311cbf84b46d1369
Nabih <estefan.nabih@gmail.com>
2022-02-21 23:53:08 -0500

    day 12 solution

--- 2017/day12/day12.py
+++ 2017/day12/day12.py
@@ -1,3 +1,26 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f:
+    lines = [a.strip().split("<->") for a in f.readlines()]
+
+points = dict()
+for start, end in lines: points[int(start)] = list(map(int, end.split(",")))
+
+def run(part1):
+    groups = 0
+    while len(points) > 0:
+        visited = set()
+        visited.add(list(points.keys())[0])
+        added = True
+        while added:
+            added = False
+            for i in set(points.keys()) - visited:
+                if len(visited & set(points[i])) > 0:
+                    visited.add(i)
+                    added = True
+
+        if part1: return len(visited)
+        for i in list(visited): del points[i]
+        groups += 1
+    return groups
+
+print("Part 1: ", run(True))
+print("Part 2: ", run(False))
