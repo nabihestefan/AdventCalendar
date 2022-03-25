13590dce9e4bf00b407f09565671c1a76f3fe129
Nabih <estefan.nabih@gmail.com>
2022-03-25 00:13:10 -0400

    solution day 21

--- 2017/day20/day20.py
+++ 2017/day20/day20.py
@@ -1,3 +1,40 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f: lines =  [a.strip() for a in f.readlines()]
+
+class Particle:
+    def __init__(self, p, v, a):
+        self.p = p
+        self.v = v
+        self.a = a
+
+    def update(self):
+        for i in range(3):
+            self.v[i] += self.a[i]
+            self.p[i] += self.v[i]
+
+particles = []
+for i in lines:
+    pos, vel, acc = i.split("<")[1:]
+    pos = map(int, pos[0:pos.index(">")].split(","))
+    vel = map(int, vel[0:vel.index(">")].split(","))
+    acc = map(int, acc[0:acc.index(">")].split(","))
+    particles.append(Particle(pos,vel,acc))
+
+def part1():
+    manAccs = [sum(map(abs, i.a)) for i in particles]
+    return manAccs.index(min(manAccs))
+
+def part2():
+    for step in range(100):
+        positions = dict()
+        for i in particles:
+            i.update()
+            positions[tuple(i.p)] = positions.get(tuple(i.p), []) + [i]
+
+        for i in positions:
+            if len(positions[i]) > 1:
+                for p in positions[i]: particles.remove(p)
+
+    return len(particles)
+
+print("Part 1: ", part1())
+print("Part 2: ", part2())
--- 2017/day21/day21.py
+++ 2017/day21/day21.py
@@ -1,3 +1,86 @@
-files = ['input.txt', 'inputTest.txt']
-## Parsing
-with open(files[0], 'r') as f: lines =  [a.strip() for a in f.readlines()]
+with open('input.txt', 'r') as f: lines =  [a.strip() for a in f.readlines()]
+
+insts2 = dict()
+insts3 = dict()
+
+def rotate(section):
+    if len(section) == 4:
+        return [section[2], section[0],
+                section[3], section[1]]
+
+    return [section[6], section[3], section[0],
+            section[7], section[4], section[1],
+            section[8], section[5], section[2]]
+
+def flip(section):
+    if len(section) == 4:
+        return [section[1], section[0],
+                section[3], section[2]]
+
+    return [section[2], section[1], section[0],
+            section[5], section[4], section[3],
+            section[8], section[7], section[6]]
+
+for i in lines:
+    og, to = i.strip().split("=>")
+    start = []
+    end = []
+    for i in og:
+        if i == ".": start.append(False)
+        elif i == "#": start.append(True)
+    for i in to:
+        if i == ".": end.append(False)
+        elif i == "#": end.append(True)
+    if len(start) == 4: insts2[tuple(start)] = end
+    elif len(start) == 9: insts3[tuple(start)] = end
+
+insts = {2: insts2, 3:insts3}
+
+def run(steps):
+    grid = [[False, True, False], [False, False, True], [True, True, True]]
+    for step in range(steps):
+        #For every step
+        next = []
+        fixed = False
+        for val in [2,3]:
+            # Checked divisble by 2 or 3
+            if len(grid)%val == 0 and not fixed:
+                # If its divisible and it hasnt been fixed already
+                fixed = True
+                for row in range(len(grid)//val):
+                    # For every row section
+                    for i in range(val+1): next.append([])
+                    # Create blank row in next grid
+                    for col in range(len(grid)//val):
+                        # For every column section
+                        tempGrid = []
+                        # Create temporary grid for key
+                        for i in range(val):
+                            tempGrid.append(grid[(row*val)+i][(col*val):(col*val)+val])
+                        # Collapse key into single dim array
+                        section = sum(tempGrid, [])
+                        # Rotate and flip until you find key configuration
+                        for rot in range(8):
+                            if tuple(section) in insts[val].keys():
+                                nextSec = insts[val][tuple(section)]
+                                break
+                            if rot == 4: section = flip(section)
+                            else: section = rotate(section)
+                        # Once found go through the array and add the new values
+                        for i in
+                        nextGrid = [] range(val+1):
+                            next[(row*(val+1))+i] += nextSec[i*(val+1): i*(val+1)+(val+1)]
+
+        # If youre ending dont copy to grid, not worth it
+        if step == steps-1:
+            return sum(sum(next, []))
+
+        # Copy to grid for next iteration
+        grid = next[:]
+
+def part2():
+
+    return None
+
+print("Part 1: ", run(5))
+print("Part 2: ", run(18))
