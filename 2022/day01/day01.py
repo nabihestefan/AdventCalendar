1671751eade5c000fe42181c9ecff1e61f2720db
Nabih Estefan <nabihestefan@Nabihs-MBP.olin.edu>
2022-12-01 13:35:20 -0500

    Cleanup day 1

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -1,17 +1,11 @@
 with open('input.txt', 'r') as f:
-    data = [x for x in f.readlines()]
-
+    data = [x.split("\n") for x in f.read().split("\n\n")]
 
 elves = []
-val = 0
-total = 0
-for i in data:
-    if i == "\n":
-        elves.append(val)
-        val = 0
-    else:
-        val += int(i)
+for elf in data: elves.append(sum(map(int, elf)))
 print("Part 1: ", max(elves))
+
+total = 0
 for i in range(3): total += elves.pop(elves.index(max(elves)))
 print("Part 2: ", total)
     
