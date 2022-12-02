18eac5a99707b20e90734f3b274783643239b6d4
Nabih Estefan <nabihestefan@Nabihs-MBP.olin.edu>
2022-12-02 00:46:20 -0500

    day02 solution

--- Python/2022/day02/day02.py
+++ Python/2022/day02/day02.py
@@ -1,10 +1,28 @@
-files = ['../input.txt', '../inputTest.txt']
+files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    data = [int(x) for x in f.readlines()]
+    data = [x.replace(" ", "") for x in f.read().split("\n")]
 
 def run(partTwo, data):
-    return None
-
+    moves = {"X":1, "Y":2, "Z":3}
+    if partTwo:
+        states = {"AX": "Z", "BY": "Y", "CZ": "X",
+                  "AY": "X", "BZ": "Z", "CX": "Y",
+                  "AZ": "Y", "BX": "X", "CY": "Z"}
+        results = {"X":0, "Y":3, "Z":6}
+    else:
+        states = {"AX": 3, "BY": 3, "CZ": 3,
+                  "AY": 6, "BZ": 6, "CX": 6,
+                  "AZ": 0, "BX": 0, "CY": 0}
+    
+    total = 0
+    for i in data:
+        if partTwo: 
+            total += moves[states[i]]
+            total += results[i[1]]
+        else: 
+            total += states[i]
+            total += moves[i[1]]
+    return total
 
 print("Part 1: ", run(False, data))
 print("Part 2: ", run(True, data))
