ec47ecdad5fe0242e039b60bfeaa9cdc91955c60
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-03 01:17:59 -0500

    small fix for day4

--- Python/2022/day03/day03.py
+++ Python/2022/day03/day03.py
@@ -3,21 +3,19 @@ with open(files[0], 'r') as f:
     data = [x.strip() for x in f.readlines()]
 
 def run(partTwo, data):
-    saved = ""
+    total = 0
     if not partTwo:
         for i in data:
-            saved += str(set(i[0:(len(i)//2)]).intersection(set(i[(len(i)//2):])).pop())
+            char = str(set(i[0:(len(i)//2)]).intersection(set(i[(len(i)//2):])).pop())
+            if char.islower(): total += ord(char) - ord("a") + 1
+            else: total += ord(char) - ord("A") + 27
     else:
         for i in range(len(data)//3):
-            saved += str(set(data[(i*3)]).intersection(set(data[(i*3)+1]), set(data[(i*3)+2])).pop())
-
-    total = 0
-    for i in saved:
-        if i.islower(): total += ord(i) - ord("a") + 1
-        else: total += ord(i) - ord("A") + 27
+            char = str(set(data[(i*3)]).intersection(set(data[(i*3)+1]), set(data[(i*3)+2])).pop())
+            if char.islower(): total += ord(char) - ord("a") + 1
+            else: total += ord(char) - ord("A") + 27
 
     return total
-
 
 print("Part 1: ", run(False, data))
 print("Part 2: ", run(True, data))
