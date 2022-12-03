ab64469c834e34901dd66f1445e01f7fda953f3f
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-03 00:27:59 -0500

    day 3 solution

--- Python/2022/day03/day03.py
+++ Python/2022/day03/day03.py
@@ -1,9 +1,22 @@
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    data = [int(x) for x in f.readlines()]
+    data = [x.strip() for x in f.readlines()]
 
 def run(partTwo, data):
-    return None
+    saved = ""
+    if not partTwo:
+        for i in data:
+            saved += str(set(i[0:(len(i)//2)]).intersection(set(i[(len(i)//2):])).pop())
+    else:
+        for i in range(len(data)//3):
+            saved += str(set(data[(i*3)]).intersection(set(data[(i*3)+1]), set(data[(i*3)+2])).pop())
+
+    total = 0
+    for i in saved:
+        if i.islower(): total += ord(i) - ord("a") + 1
+        else: total += ord(i) - ord("A") + 27
+
+    return total
 
 
 print("Part 1: ", run(False, data))
