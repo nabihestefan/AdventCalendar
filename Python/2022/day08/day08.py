48c51f1e42fbf4c554821d730f8cd43f3c5da675
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-08 10:32:14 -0500

    Slight cleanups

--- Python/2022/day08/day08.py
+++ Python/2022/day08/day08.py
@@ -2,46 +2,38 @@ files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
     data = [list(map(int, list(x.strip()))) for x in f.readlines()]
 
-def getScore(i,j):
-    left = [data[i][x] for x in range(j+1)][::-1][1:]
-    right = [data[i][x] for x in range(j, len(data[0]))][1:]
-    above = [data[x][j] for x in range(i+1)][::-1][1:]
-    below = [data[x][j] for x in range(i, len(data))][1:]
-
-    views = [left, right, above, below]
-    score = 1
-
-    for view in views:
-        curLen = 1
-        for ind,t in enumerate(view[:-1]):
-            if t >= data[i][j]:
-                score *= ind+1
-                break
-            if ind+2 == len(view):
-                score *= ind+2
-
-    return score
-
-def isVisible(i,j):
-    left = [data[i][x] for x in range(j+1)]
-    right = [data[i][x] for x in range(j, len(data[0]))]
-    above = [data[x][j] for x in range(i+1)]
-    below = [data[x][j] for x in range(i, len(data))]
-
-    views = [left, right, above, below]
-    for view in views:
-        if data[i][j] == max(view) and view.count(data[i][j]) == 1:
-            return 1
-    return 0
-
 def run(partTwo, data):
     answer = (len(data[0])*2 + len(data)*2 - 4) if not partTwo else 1
     for i, row in enumerate(data[1:-1]):
         i+=1
         for j, val in enumerate(row[1:-1]):
             j+=1
-            if not partTwo: answer += isVisible(i, j)
-            else: answer = max(answer, getScore(i,j))
+            curTree = data[i][j]
+            score = 1
+
+            left = [data[i][x] for x in range(j+1)]
+            right = [data[i][x] for x in range(j, len(data[0]))]
+            above = [data[x][j] for x in range(i+1)]
+            below = [data[x][j] for x in range(i, len(data))]
+            views = [left, right, above, below]
+
+            for ind, view in enumerate(views):
+                if not partTwo: 
+                    if curTree == max(view) and view.count(curTree) == 1:
+                        answer += 1 
+                        break
+                else:
+                    if ind % 2 == 0: view = view[::-1]
+                    view = view[1:]
+
+                    for indT, tree in enumerate(view[:-1]):
+                        if tree >= curTree:
+                            score *= indT+1
+                            break
+                        if indT+2 == len(view):
+                            score *= indT+2
+
+            answer = max(answer, score)
     
     return answer
 
