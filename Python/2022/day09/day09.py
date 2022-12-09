8373faa11ee7d83932c8bb3066a6b8cce5b2c4c2
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-09 08:46:13 -0500

    reworked day9 with complex number coorinate system to attempt to actually lern to use it

--- Python/2022/day09/day09.py
+++ Python/2022/day09/day09.py
@@ -2,39 +2,36 @@ files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
     data = [x.split() for x in f.readlines()]
 
-dirs = {"R": (0,1), "L":(0,-1), "U": (1,0), "D":(-1,0)}
+# dirs = {"R": (0,1), "L":(0,-1), "U": (1,0), "D":(-1,0)}
+dirs = {"R": 0+1j, "L":0-1j, "U": 1, "D":-1}
 
 def inRange(head,tail):
-    di = dj = 0
-    difi, difj = head[0]-tail[0], head[1]-tail[1]
+    movement = 0 + 0j
+    dif = head-tail
 
-    if abs(difi) > 1:
-        if difj != 0: dj = (difj/abs(difj))
-        di = (difi/abs(difi))
-    elif abs(difj) > 1:
-        if difi != 0: di = (difi/abs(difi))
-        dj = (difj/abs(difj))
-        
-    return di, dj
+    if abs(dif.real)>1:
+        if dif.imag != 0: movement += complex(0, (dif.imag/abs(dif.imag)))
+        movement += (dif.real/abs(dif.real))
+    elif abs(dif.imag)>1:
+        if dif.real != 0: movement += (dif.real/abs(dif.real))
+        movement += complex(0, (dif.imag/abs(dif.imag)))
 
+    return movement
+    
 def run(partTwo, data):
     tailPos = set()
     if not partTwo: ropeLen = 2
     else: ropeLen = 10
 
-    rope = [[0,0] for _ in range(ropeLen)]
-    tailPos.add(tuple(rope[-1]))
+    rope = [complex() for _ in range(ropeLen)]
+    tailPos.add(rope[-1])
 
     for dir, size in data:
         for step in range(int(size)):
-            rope[0][0] += dirs[dir][0]
-            rope[0][1] += dirs[dir][1]
+            rope[0] += dirs[dir]
             for r in range(1, ropeLen):
-                dx,dy = inRange(rope[r-1], rope[r])
-                rope[r][0] += dx
-                rope[r][1] += dy
-            tailPos.add(tuple(rope[-1]))
-
+                rope[r] += inRange(rope[r-1], rope[r])
+            tailPos.add(rope[-1])
     return len(tailPos)
 
 print("Part 1: ", run(False, data))
