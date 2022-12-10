5e5d04ad2502d7ab16e3448cc1fd3264441bf293
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-10 00:45:46 -0500

    day 10 solution

--- Python/2022/day10/day10.py
+++ Python/2022/day10/day10.py
@@ -1,10 +1,28 @@
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    data = [int(x) for x in f.readlines()]
+    data = [x.strip().split() for x in f.readlines()]
 
-def run(partTwo, data):
-    return None
+def run(data):
+    pos = 1
+    cycle = 0
+    signalStrength = 0
+    measured = [20, 60, 100, 140, 180, 220]
+    board = [["." for _ in range(40)] for _ in range(6)]
+    
+    for ind, i in enumerate(data):
+        
+        cycle += 1
+        if abs(pos-(cycle%40)) <= 1: board[cycle//40][cycle%40] = "#"
+        if cycle+1 in measured: signalStrength += (pos * (cycle+1))
 
+        if len(i) == 2:  
+            cycle += 1
+            pos += int(i[1])
+            if abs(pos-(cycle%40)) <= 1: board[cycle//40][cycle%40] = "#"
+            if cycle+1 in measured: signalStrength += (pos * (cycle+1))
+        
+    print("Part 1: ", signalStrength)
+    print("Part 2: ")
+    for row in board: print(" ".join(row))
 
-print("Part 1: ", run(False, data))
-print("Part 2: ", run(True, data))
+run(data)
