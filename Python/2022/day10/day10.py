be428fdc0bd52e1912d6c35c58f5bef5e88c0e93
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-10 01:02:18 -0500

    changed part 2 for readability

--- Python/2022/day10/day10.py
+++ Python/2022/day10/day10.py
@@ -7,22 +7,24 @@ def run(data):
     cycle = 0
     signalStrength = 0
     measured = [20, 60, 100, 140, 180, 220]
-    board = [["." for _ in range(40)] for _ in range(6)]
+    board = [[" " for _ in range(40)] for _ in range(6)]
+    
+    if abs(pos-(cycle%40)) <= 1: board[cycle//40][cycle%40] = "█"
+    if cycle+1 in measured: signalStrength += (pos * (cycle+1))
     
     for ind, i in enumerate(data):
-        
         cycle += 1
-        if abs(pos-(cycle%40)) <= 1: board[cycle//40][cycle%40] = "#"
+        if abs(pos-(cycle%40)) <= 1: board[cycle//40][cycle%40] = "█"
         if cycle+1 in measured: signalStrength += (pos * (cycle+1))
 
         if len(i) == 2:  
             cycle += 1
             pos += int(i[1])
-            if abs(pos-(cycle%40)) <= 1: board[cycle//40][cycle%40] = "#"
+            if abs(pos-(cycle%40)) <= 1: board[cycle//40][cycle%40] = "█"
             if cycle+1 in measured: signalStrength += (pos * (cycle+1))
         
     print("Part 1: ", signalStrength)
     print("Part 2: ")
-    for row in board: print(" ".join(row))
+    for row in board: print("".join(row))
 
 run(data)
