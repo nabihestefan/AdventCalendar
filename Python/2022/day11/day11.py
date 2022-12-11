1ea73deee6b95688d01465fb6aed779acc5c7149
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-11 14:17:40 -0500

    day 11 solution

--- Python/2022/day11/day11.py
+++ Python/2022/day11/day11.py
@@ -1,10 +1,58 @@
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    data = [int(x) for x in f.readlines()]
+    monkeyData = [x.split("\n") for x in f.read().split("\n\n")]
 
-def run(partTwo, data):
-    return None
+class Monkey:
+    def __init__(self, items, operation, test):
+        self.inspected = 0
+        self.items = [int(i) for i in items[18:].split(',')]
+
+        operation = operation.split()
+        if operation[4] == "+":
+            if operation[5] == "old":
+                self.operation = lambda x: x+x
+            else:
+                self.operation = lambda x: x+int(operation[5])
+        elif operation[4] == "*":
+            if operation[5] == "old":
+                self.operation = lambda x: x*x
+            else:
+                self.operation = lambda x: x*int(operation[5])
+
+        self.test = int(test[0].split()[3])
+        self.true = int(test[1].split()[5])
+        self.false = int(test[2].split()[5])
+
+def run(partTwo, monkeyData):
+
+    monkeys = []
+    LCM = 1
+    for data in monkeyData:
+        monkeys.append(Monkey(data[1], data[2], data[3:]))
+        if LCM % monkeys[-1].test != 0: LCM *= monkeys[-1].test
+    print(LCM)
+    steps = 20 if not partTwo else 10000
+    for step in range(steps):
+        for monkey in monkeys:
+            monkey.inspected +=len(monkey.items)
+
+            for item in monkey.items:
+                item = monkey.operation(item)
+                if not partTwo: item //= 3
+                else: item %= LCM
+
+                if item % monkey.test == 0:
+                    monkeys[monkey.true].items.append(item)
+                else:
+                    monkeys[monkey.false].items.append(item)
+            monkey.items = []
+        print("STEP ", step)
+        # for m in monkeys:
+        #     print(m.items)
+
+    monkeys = sorted(monkeys, key=lambda monkey: monkey.inspected, reverse=True)
+    return monkeys[0].inspected * monkeys[1].inspected
 
 
-print("Part 1: ", run(False, data))
-print("Part 2: ", run(True, data))
+print("Part 1: ", run(False, monkeyData))
+print("Part 2: ", run(True, monkeyData))
