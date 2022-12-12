26ad243ba548447b996e2ff7df2b58e58eaf265b
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-12 11:50:26 -0500

    day 12 solution

--- Python/2022/day12/day12.py
+++ Python/2022/day12/day12.py
@@ -1,10 +1,47 @@
+import networkx as nx
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    data = [int(x) for x in f.readlines()]
+    data = [list(x.strip()) for x in f.readlines()]
 
-def run(partTwo, data):
-    return None
+map = []
+g = nx.DiGraph()
+p2Start = []
 
+def getVal(char):
+    if char == "S": return 0
+    elif char == "E": return 25
+    else: return ord(char) - ord("a")
 
-print("Part 1: ", run(False, data))
-print("Part 2: ", run(True, data))
+for x,row in enumerate(data):
+    for y, i in enumerate(row):
+        if i == "S": start = (x,y)
+        elif i == "E":  end = (x,y)
+        if i == "S" or i == "a": p2Start.append((x,y))
+
+        val = getVal(i)
+        g.add_node((x,y))
+
+        difs = [(1,0), (0,1), (-1,0), (0,-1)]
+        for dx, dy in difs:
+            newX = x-dx
+            newY = y-dy
+            if 0 <= newX and newX < len(data) and 0 <= newY and newY < len(data[0]):
+                if getVal(data[newX][newY]) - val <= 1:
+                    g.add_edge((x,y), (newX, newY))
+
+def run(partTwo, g, start, end, p2Start):
+    if not partTwo:
+        paths = [len(nx.shortest_path(g, start, end))]
+    else:
+        paths = []
+        for s in p2Start:
+            try:
+                path = nx.shortest_path(g, s, end)
+                paths.append(len(path))
+            except:
+                continue
+            
+    return min(paths)-1
+    
+print("Part 1: ", run(False, g, start, end, p2Start))
+print("Part 2: ", run(True, g, start, end, p2Start))
