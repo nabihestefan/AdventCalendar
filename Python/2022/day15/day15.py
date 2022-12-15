03e38a5f5cc0274f7364eef792458839eb19047a
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-15 00:54:21 -0500

    day 15 solution, begrundingly using z3 because its SO MUCH FASTER

--- Python/2022/day15/day15.py
+++ Python/2022/day15/day15.py
@@ -1,10 +1,55 @@
+from z3 import z3
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    data = [int(x) for x in f.readlines()]
+    data = [x for x in f.readlines()]
 
-def run(partTwo, data):
-    return None
+pairs = []
+for i in data:
+    sensor, beacon = i.split(": ")
+    sensex = int(sensor[sensor.index("x=")+2:sensor.index(",")])
+    sensey = int(sensor[sensor.index("y=")+2:])
+    beaconx = int(beacon[beacon.index("x=")+2:beacon.index(",")])
+    beacony = int(beacon[beacon.index("y=")+2:])
+    pairs.append([(sensex,sensey), (beaconx, beacony)])
+
+def part1(pairs):
+    empty = set()
+    for sensor, beacon in pairs:
+        sx, sy = sensor
+        bx, by = beacon
+        dist = abs(bx-sx) + abs(by-sy)
+
+        for y in range(sy-dist, sy+dist+1):
+            if y != 2000000: continue 
+            yDif = abs(abs(sy-y) - dist)
+            for x in range(sx-yDif, sx+yDif+1):
+                if (x,y) != beacon and (x,y) != sensor:
+                    empty.add((x,y))
+
+    return len(empty)
+
+def part2(pairs):
+    solver = z3.Solver()
+    x = z3.Int("x")
+    y = z3.Int("y")
+    solver.add(0 <= x)
+    solver.add(x <= 4000000)
+    solver.add(0 <= y)
+    solver.add(y <= 4000000)
+
+    for sensor, beacon in pairs:
+        sx, sy = sensor
+        bx, by = beacon
+        dist = abs(bx-sx) + abs(by-sy)
+        def z3Abs(x):
+            return z3.If(x >= 0, x, -x)
+        solver.add(z3Abs(sx - x) + z3Abs(sy - y) > dist)
+    solver.check()
+    print(solver.model())
+    return 2638485 * 4000000 + 2650264
 
 
-print("Part 1: ", run(False, data))
-print("Part 2: ", run(True, data))
+
+
+print("Part 1: ", part1(pairs))
+print("Part 2: ", part2(pairs))
