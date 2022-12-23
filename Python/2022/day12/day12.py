dfb5fae7ba2b515a0d4dee04f82d63208dd4a405
Nabih Estefan <estfean.nabih@gmail.com>
2022-12-23 14:50:00 -0500

    day12 solution with no libraries

--- Python/2022/day12/day12.py
+++ Python/2022/day12/day12.py
@@ -1,46 +1,51 @@
-import networkx as nx
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
     data = [list(x.strip()) for x in f.readlines()]
-
-g = nx.DiGraph()
-p2Start = []
+g = []
 
 def getVal(char):
     if char == "S": return 0
     elif char == "E": return 25
     else: return ord(char) - ord("a")
 
-for x,row in enumerate(data):
-    for y, i in enumerate(row):
+for y,row in enumerate(data):
+    r = []
+    for x,i in enumerate(row):
         if i == "S": start = (x,y)
         elif i == "E":  end = (x,y)
-        if i == "S" or i == "a": p2Start.append((x,y))
+        r.append(getVal(i))
+    g.append(r)
 
-        val = getVal(i)
-        g.add_node((x,y))
+def run(partTwo, g, start, end):
+    if not partTwo: queue = [(start, 0)]
+    else: queue = [(end, 0)]
 
+    visited = set([start])
+    minSteps = int(1e20)
+
+    while queue:
+        (x,y),steps = queue.pop(0)
         difs = [(1,0), (0,1), (-1,0), (0,-1)]
-        for dx, dy in difs:
-            newX = x-dx
-            newY = y-dy
-            if 0 <= newX and newX < len(data) and 0 <= newY and newY < len(data[0]):
-                if getVal(data[newX][newY]) - val <= 1:
-                    g.add_edge((x,y), (newX, newY))
+        val = g[y][x]
 
-def run(partTwo, g, start, end, p2Start):
-    if not partTwo:
-        paths = [len(nx.shortest_path(g, start, end))]
-    else:
-        paths = []
-        for s in p2Start:
-            try:
-                path = nx.shortest_path(g, s, end)
-                paths.append(len(path))
-            except:
-                continue
-            
-    return min(paths)-1
-    
-print("Part 1: ", run(False, g, start, end, p2Start))
-print("Part 2: ", run(True, g, start, end, p2Start))
+        if not partTwo:
+            if (x,y) == end: minSteps = min(minSteps,steps)
+        else:
+            if val == 0: minSteps = min(minSteps,steps)
+
+        for dx,dy in difs:
+            newX = x+dx
+            newY = y+dy
+            if (newX,newY) in visited: continue
+            if 0 <= newX and newX < len(data[0]) and 0 <= newY and newY < len(data):
+                if g[newY][newX] - val <= 1 and not partTwo:
+                    visited.add((newX,newY))
+                    queue.append(((newX,newY), steps+1))
+                elif val - g[newY][newX] <= 1 and  partTwo:
+                    visited.add((newX,newY))
+                    queue.append(((newX,newY), steps+1))
+                    
+    return minSteps
+
+print("Part 1: ", run(False, g, start, end))
+print("Part 2: ", run(True, g, start, end))
