929feba4e6fbeae8d01b8a1fe397ff107c10186a
Nabih Estefan <estfean.nabih@gmail.com>
2024-12-01 21:33:46 -0800

    Day 2 solution. Took me longer than I would have wanted, basically deleted and restarted 20 mins in. It works thought and it looks nice.

--- 2024/day02/day.py
+++ 2024/day02/day.py
@@ -1,12 +1,28 @@
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    lines = [x.strip() for x in f.readlines()]
+    data = [map(int, x.strip().split(" ")) for x in f.readlines()]
 
-data = lines
+def is_safe(line):
+    increasing = True if (line[0] - line[1] < 0) else False
+    for i in range(len(line)-1):
+            cur = line[i]
+            nxt = line[i+1]
+            if (increasing and cur > nxt) or (not increasing and cur < nxt):
+                return False
+            if abs(cur-nxt) < 1 or 3 < abs(cur-nxt):
+                return False
+    return True
 
 def run(data, partTwo=False):
-    
-    return None
+    levelSafe = 0
+    for i in data:
+        if is_safe(i): levelSafe += 1
+        elif partTwo:
+             for idx in range(len(i)):
+                if is_safe(i[:idx] + i[idx+1:]):
+                    levelSafe += 1
+                    break
+    return levelSafe
     
 print("Part 1: ", run(data))
 print("Part 2: ", run(data, True))
