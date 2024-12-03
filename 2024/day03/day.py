afaa5605d19d197e04c4955b3f5b8df371cb095d
Nabih Estefan <estfean.nabih@gmail.com>
2024-12-02 21:37:48 -0800

    Day 3 solution. Giving in to using regex even though I have to re-learn it every time I use it.

--- 2024/day03/day.py
+++ 2024/day03/day.py
@@ -1,12 +1,16 @@
+import re
 files = ['input.txt', 'inputTest.txt']
-with open(files[0], 'r') as f:
-    lines = [x.strip() for x in f.readlines()]
+with open(files[0], 'r') as f: data = f.read()
 
-data = lines
-
-def run(data, partTwo=False):
+def run(data):
+    p1 = p2 = 0
+    execute = True
+    for i,j,k in re.findall(r'mul\((\d{1,3})\,(\d{1,3})\)|(don\'t\(\)|do\(\))', data):
+        if k == "don't()": execute = False
+        elif k == "do()": execute = True
+        else:
+            p1 += int(i) * int(j)
+            if execute: p2 += int(i) * int(j)
+    return p1, p2
     
-    return None
-    
-print("Part 1: ", run(data))
-print("Part 2: ", run(data, True))
+print("Part 1&2: ", run(data))
