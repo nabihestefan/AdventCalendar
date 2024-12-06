35eab8551091c0bbc4954c56aed7da11d2065618
Nabih Estefan <estfean.nabih@gmail.com>
2024-12-05 22:21:05 -0800

    Day 6. Tried to math out p2 and lost my lead. Should have commited to a brute force and let it run while I thought, it runs so fast its not worth doing anything else. Once I committed to brute force-ish I got it pretty quick

--- 2024/day06/day.py
+++ 2024/day06/day.py
@@ -1,12 +1,32 @@
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    lines = [x.strip() for x in f.readlines()]
+    data = [x.strip() for x in f.readlines()]
 
-data = lines
+blocks = set([complex(x, y) for y in range(len(data)) for x in range(len(data[y])) if data[y][x] == '#'])
+start = [complex(x,y) for y in range(len(data)) for x in range(len(data[y])) if data[y][x] == '^'][0]
+MAX_X = len(data[0])
+MAX_Y = len(data)
 
-def run(data, partTwo=False):
+def transit(pos, dir, block=-1):
+    visited = set()
+    path = dict()
+    while pos.imag < MAX_Y and pos.imag >= 0 and pos.real < MAX_X and pos.real >= 0:
+        if pos+dir in blocks or pos+dir == block:
+            dir *= 1j
+        else: 
+            if pos in visited and dir in path[pos]: return True, visited
+            visited.add(pos)
+            path[pos] = path.get(pos, []) + [dir]
+            pos += dir
+    return False, visited
+
+def run(blocks, start):
+    pos, dir = start, -1j
+    _, visited = transit(pos,dir)
+
+    p2 = 0
+    for v in visited: p2 += transit(start, -1j,v)[0]
+        
+    return len(visited), p2
     
-    return None
-    
-print("Part 1: ", run(data))
-print("Part 2: ", run(data, True))
+print("Part 1&2: ", run(blocks, start))
--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Any other languages I used were used to learn while doing problems I knew I unde
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  Advent of Code - 460/460 ⭐
+  Advent of Code - 462/462 ⭐
 </h1>
 <h1 align="center">
-  2024 - 10 ⭐ - Python
+  2024 - 12 ⭐ - Python
 </h1>
 <a href="2024/day01/day.py">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -26,6 +26,9 @@ Any other languages I used were used to learn while doing problems I knew I unde
 </a>
 <a href="2024/day05/day.py">
   <img src=".aoc_tiles/tiles/2024/05.png" width="161px">
+</a>
+<a href="2024/day06/day.py">
+  <img src=".aoc_tiles/tiles/2024/06.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐ - Python
