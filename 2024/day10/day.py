8dd3f8800e9ce4c28ef35be76505a92357b4d485
Nabih Estefan <estfean.nabih@gmail.com>
2024-12-09 21:24:51 -0800

    Day 10. Accidentally implemented p2 when doing p1 cause I can't read. It was very helpful for solving p2 once I realized but I lost previous minutes there

--- 2024/day10/day.py
+++ 2024/day10/day.py
@@ -1,12 +1,27 @@
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    lines = [x.strip() for x in f.readlines()]
+    data = [map(int,x.strip()) for x in f.readlines()]
 
-data = lines
+def findPaths(data, start, partTwo):
+    paths = []
+    stack = [start]
+    while stack:
+        x,y = stack.pop(0)
+        if data[y][x] == 9:
+            paths.append((x,y))
+            continue
+        for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+            nx, ny = x+dx, y+dy
+            if 0 <= nx < len(data[0]) and 0 <= ny < len(data) and data[ny][nx] == data[y][x]+1:
+                stack.append((nx,ny))
+    return len(paths) if partTwo else len(set(paths))
 
 def run(data, partTwo=False):
-    
-    return None
+    starts = [(x,y) for x in range(len(data[0])) for y in range(len(data)) if data[y][x] == 0]
+    paths = 0
+    for s in starts:
+        paths += findPaths(data,s,partTwo)    
+    return paths
     
 print("Part 1: ", run(data))
 print("Part 2: ", run(data, True))
--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Any other languages I used were used to learn while doing problems I knew I unde
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  Advent of Code - 468/468 ⭐
+  Advent of Code - 470/470 ⭐
 </h1>
 <h1 align="center">
-  2024 - 18 ⭐ - Python
+  2024 - 20 ⭐ - Python
 </h1>
 <a href="2024/day01/day.py">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -38,6 +38,9 @@ Any other languages I used were used to learn while doing problems I knew I unde
 </a>
 <a href="2024/day09/day.py">
   <img src=".aoc_tiles/tiles/2024/09.png" width="161px">
+</a>
+<a href="2024/day10/day.py">
+  <img src=".aoc_tiles/tiles/2024/10.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐ - Python
