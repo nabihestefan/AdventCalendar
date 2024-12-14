60b99ebe7a9a660d3ce151dc420a01033877e017
Nabih Estefan <estfean.nabih@gmail.com>
2024-12-14 02:04:14 -0800

    Day 14. P2 was interesting, not my vibe tbh. I did create a pretty print to figure it out so I tweaked it so it at least prints a nice tree with my input when I run it.

--- 2024/day14/day.py
+++ 2024/day14/day.py
@@ -1,12 +1,44 @@
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    lines = [x.strip() for x in f.readlines()]
+    data = [x.strip() for x in f.readlines()]
 
-data = lines
+robots = []
+for i in data:
+    pos, vel = i.split(" ")
+    robots.append([list(map(int, pos.split("=")[1].split(","))), list(map(int, vel.split("=")[1].split(",")))])
 
-def run(data, partTwo=False):
+def moveRobot(pos, vel, amount=1):
+    np = pos[0] + vel[0]*amount, pos[1] + vel[1]*amount
+    return (np[0]%101, np[1]%103), vel
+
+def prettyPrint(robots, minx=0, maxx=101, miny=0, maxy=103):
+    grid = [[" " for _ in range(101)] for _ in range(103)]
+    for x,y in robots:
+        grid[y][x] = "#"
+    for row in grid[miny:maxy]:
+        print("".join(row[minx:maxx]))
+
+def run(robots):
+    newRobots = [moveRobot(pos,vel,100)[0] for pos,vel in robots]
+    X_LINE = 50
+    Y_LINE = 51
+    q1 = q2 = q3 = q4 = 0
+    for x,y in newRobots:
+        if x < X_LINE and y < Y_LINE: q1 += 1
+        elif x < X_LINE and y > Y_LINE: q2 += 1
+        elif x > X_LINE and y < Y_LINE: q3 += 1
+        elif x > X_LINE and y > Y_LINE: q4 += 1
+
+    i = 100
+    while True:
+        robots = [moveRobot(pos,vel) for pos,vel in robots]
+        r = [x[0] for x in robots]
+        i += 1
+        if len(r) == len(set(r)): break
     
-    return None
+    prettyPrint(r, 38, 69, 17, 50)
+    return q1*q2*q3*q4, i
     
-print("Part 1: ", run(data))
-print("Part 2: ", run(data, True))
+
+print("Part 1: ", run(robots))
+# print("Part 2: ", p2(robots))
--- README.md
+++ README.md
@@ -7,10 +7,10 @@ Any other languages I used were used to learn while doing problems I knew I unde
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  Advent of Code - 476/476 ⭐
+  Advent of Code - 478/478 ⭐
 </h1>
 <h1 align="center">
-  2024 - 26 ⭐ - Python
+  2024 - 28 ⭐ - Python
 </h1>
 <a href="2024/day01/day.py">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -50,6 +50,9 @@ Any other languages I used were used to learn while doing problems I knew I unde
 </a>
 <a href="2024/day13/day.py">
   <img src=".aoc_tiles/tiles/2024/13.png" width="161px">
+</a>
+<a href="2024/day14/day.py">
+  <img src=".aoc_tiles/tiles/2024/14.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐ - Python
