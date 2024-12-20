4709835f66f432c18257b9a899f8dfc3b4e9f7da
Nabih Estefan <estfean.nabih@gmail.com>
2024-12-19 22:27:50 -0800

    Day 19. Simpler than what I would have expected this late into the month but still fun

--- 2024/day19/day.py
+++ 2024/day19/day.py
@@ -1,12 +1,27 @@
 files = ['input.txt', 'inputTest.txt']
 with open(files[0], 'r') as f:
-    lines = [x.strip() for x in f.readlines()]
+    data = [x.strip() for x in f.readlines()]
 
-data = lines
+patterns = set(data[0].split(", "))
+designs = [x.strip() for x in data[2:]]
 
-def run(data, partTwo=False):
+KNOWN = {"": 1}
+
+def designPossible(design, patterns):
+    if design in KNOWN: return KNOWN[design]
+    p = 0
+    for patt in patterns:
+        if design[:len(patt)] == patt:
+            p += designPossible(design[len(patt):], patterns)
+    KNOWN[design] = p
+    return p
+
+def run(designs, patterns):
+    p1 = p2 = 0
+    for d in designs:
+        p = designPossible(d, patterns)
+        p1 += (p > 0)
+        p2 += p
+    return p1, p2
     
-    return None
-    
-print("Part 1: ", run(data))
-print("Part 2: ", run(data, True))
+print("Part 1&2: ", run(designs, patterns))
